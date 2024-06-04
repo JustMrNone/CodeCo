7b050b1b42974dab844157f2b4c58657d53034b7
Kamyar Bashar <kamyar.bashar@gmail.com>
2024-06-04 21:34:56 +0330

    "Updated CSS styles for dark mode, adjusted card and button styles, added JavaScript for file input and arrow icon toggle, and modified HTML structure for settings page."

--- main/static/settings/js/script.js
+++ main/static/settings/js/script.js
@@ -1,10 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
     let cardHeaders = document.querySelectorAll(".card-header");
+    const fileInput = document.getElementById('id_profile_picture');
+    const fileInputText = document.querySelector('.file-input-text');
 
+    fileInput.addEventListener('change', function() {
+        if (fileInput.files.length > 0) {
+            fileInputText.textContent = fileInput.files[0].name;
+        } else {
+            fileInputText.textContent = 'No file chosen';
+        }
+    });
     cardHeaders.forEach(function (header) {
         header.addEventListener("click", function () {
-            let cardBody = this.nextElementSibling;
-            cardBody.classList.toggle("collapsed");
+          let cardBody = this.nextElementSibling;
+          cardBody.classList.toggle("collapsed");
+      
+          // Update arrow icon based on collapse state
+          const arrowIcon = this.querySelector('i.arrowdown');
+      
+          // Toggle the arrow icon classes
+          if (cardBody.classList.contains("collapsed")) {
+            arrowIcon.classList.remove('fa-arrow-down');
+            arrowIcon.classList.add('fa-arrow-up');
+          } else {
+            arrowIcon.classList.remove('fa-arrow-up');
+            arrowIcon.classList.add('fa-arrow-down');
+          }
         });
-    });
+      });
 });
+
+
